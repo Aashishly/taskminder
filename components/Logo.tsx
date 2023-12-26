b2f1ca12586c25e9c7631c08720f76329025e88f
Aashish Singh <11aashish.singh@gmail.com>
2023-12-26 19:22:02 +0530

    init

---
+++ components/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <h1 className='text-2xl font-bold bg-gradient-to-r from-[#AC87C5] to-[#E0AED0] bg-clip-text text-transparent'>TaskMinder</h1>
+  )
+}
+
+export default Logo
